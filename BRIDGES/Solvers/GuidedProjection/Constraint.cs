9335669c901e8ad5a9e855fa07d5ce0167027996
nrmontagne <nicolas.montagne@enpc.fr>
2023-08-03 10:18:11 +0200

    Major Re-organisation of the classes
    Interfaces became Abstracts; "Fusion" of Linearised|Quadratic Constraints; Refactoring of EnergyTypes and ConstraintTypes

---
+++ BRIDGES/Solvers/GuidedProjection/Constraint.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BRIDGES.Solvers.GuidedProjection.Abstracts;
+
+
+namespace BRIDGES.Solvers.GuidedProjection
+{
+    /// <summary>
+    /// Class defining a quadratic constraint for the <see cref="GuidedProjectionAlgorithm"/>.
+    /// </summary>
+    public sealed class Constraint
+    {
+        #region Fields
+
+        /// <summary>
+        /// Variables composing the local vector LocalX on which the <see cref="Type"/> is defined.
+        /// </summary>
+        private readonly List<Variable> _variables;
+
+        #endregion
+
+        #region Properties
+
+        /// /// <summary>
+        /// Gets the constraint type defining the reduced matrix <see cref="ConstraintType.LocalHi"/>, the reduced vector <see cref="ConstraintType.LocalBi"/> and the scalar value <see cref="ConstraintType.Ci"/>.
+        /// </summary>
+        public ConstraintType Type { get; private set; }
+
+        /// <summary>
+        /// Gets the variables composing the local vector LocalX.
+        /// </summary>
+        public IReadOnlyList<Variable> Variables => _variables;
+
+
+        /// <summary>
+        /// Gets or sets the value of the weight for the constraint.
+        /// </summary>
+        public double Weight { get; internal set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Constraint"/> class.
+        /// </summary>
+        /// <param name="constraintType"> Constraint type defining the local quantities of the constraint. </param>
+        /// <param name="variables"> Variables composing the local vector LocalX on which the local symmetric matrix Hi and the local vector Bi are defined.</param>
+        /// <param name="weight"> Weight of the constraint. </param>
+        internal Constraint(ConstraintType constraintType, List<Variable> variables, double weight)
+        {
+            this.Type = constraintType;
+            this._variables = variables;
+
+            Weight = weight;
+        }
+
+        #endregion
+    }
+}
--- BRIDGES/Solvers/GuidedProjection/QuadraticConstraintTypes/CoherentLength.cs
+++ BRIDGES/Solvers/GuidedProjection/QuadraticConstraintTypes/CoherentLength.cs
@@ -1,63 +1,61 @@
 ﻿using System;
 
-using BRIDGES.LinearAlgebra.Vectors;
-using BRIDGES.LinearAlgebra.Matrices;
 using BRIDGES.LinearAlgebra.Matrices.Sparse;
-using BRIDGES.Solvers.GuidedProjection.Interfaces;
+using BRIDGES.Solvers.GuidedProjection.Abstracts;
 
 
 namespace BRIDGES.Solvers.GuidedProjection.QuadraticConstraintTypes
 {
     /// <summary>
-    /// Constraint enforcing a scalar variable <em>l</em> to match with the distance between two point variables, <em>pi</em> and <em>pj</em>.
+    /// Constraint enforcing a scalar variable to match with the distance between two variables. The list of variables for this constraint consists of:
+    /// <list type="bullet">
+    ///     <item> 
+    ///         <term>P<sub>i</sub></term>
+    ///         <description> First variable with which the distance is being computed. </description>
+    ///     </item>
+    ///     <item> 
+    ///         <term>P<sub>j</sub></term>
+    ///         <description> Second variable with which the distance is being computed. </description>
+    ///     </item>
+    ///     <item> 
+    ///         <term>L</term>
+    ///         <description> Scalar variable to match with the distance. </description>
+    ///     </item>
+    /// </list>
     /// </summary>
-    /// <remarks> The vector xReduced = [pi, pj, l].</remarks>
-    public class CoherentLength : IQuadraticConstraintType
+    public class CoherentLength : ConstraintType
     {
-        #region Properties
-
-        /// <inheritdoc cref="IQuadraticConstraintType.LocalHi"/>
-        public SparseMatrix LocalHi { get; }
-
-        /// <inheritdoc cref="IQuadraticConstraintType.LocalBi"/>
-        public SparseVector LocalBi { get; }
-
-        /// <inheritdoc cref="IQuadraticConstraintType.Ci"/>
-        public double Ci { get; }
-
-        #endregion
-
         #region Constructors
 
         /// <summary>
         /// Initialises a new instance of the <see cref="CoherentLength"/> class.
         /// </summary>
-        /// <param name="spaceDimension"> Dimension of the space containing the points. </param>
-        public CoherentLength(int spaceDimension = 3)
+        /// <param name="dimension"> Common dimension of the variables with which the distance is being computed. </param>
+        public CoherentLength(int dimension)
         {
             /******************** Define LocalHi ********************/
 
-            int[] columnPointers = new int[(2 * spaceDimension) + 2];
-            int[] rowIndices = new int[(4 * spaceDimension) + 1];
-            double[] values = new double[(4 * spaceDimension) + 1];
+            int[] columnPointers = new int[(2 * dimension) + 2];
+            int[] rowIndices = new int[(4 * dimension) + 1];
+            double[] values = new double[(4 * dimension) + 1];
 
             columnPointers[0] = 0;
-            for (int i_C = 0; i_C < spaceDimension; i_C++)
+            for (int i_C = 0; i_C < dimension; i_C++)
             {
                 columnPointers[i_C + 1] = 2 * (i_C + 1);
-                rowIndices[(2 * i_C)] = i_C; rowIndices[(2 * i_C) + 1] = spaceDimension + i_C;
+                rowIndices[(2 * i_C)] = i_C; rowIndices[(2 * i_C) + 1] = dimension + i_C;
                 values[(2 * i_C)] = 2.0; values[(2 * i_C) + 1] = -2.0;
 
-                columnPointers[spaceDimension + i_C + 1] = 2 * (spaceDimension + i_C + 1);
-                rowIndices[(2 * (spaceDimension + i_C))] = i_C; rowIndices[(2 * (spaceDimension + i_C)) + 1] = spaceDimension + i_C;
-                values[(2 * (spaceDimension + i_C))] = -2.0; values[(2 * (spaceDimension + i_C)) + 1] = 2.0;
+                columnPointers[dimension + i_C + 1] = 2 * (dimension + i_C + 1);
+                rowIndices[(2 * (dimension + i_C))] = i_C; rowIndices[(2 * (dimension + i_C)) + 1] = dimension + i_C;
+                values[(2 * (dimension + i_C))] = -2.0; values[(2 * (dimension + i_C)) + 1] = 2.0;
             }
 
-            columnPointers[(2 * spaceDimension) + 1] = (4 * spaceDimension) + 1;
-            rowIndices[(4 * spaceDimension)] = (2 * spaceDimension);
-            values[(4 * spaceDimension)] = -2.0;
+            columnPointers[(2 * dimension) + 1] = (4 * dimension) + 1;
+            rowIndices[(4 * dimension)] = (2 * dimension);
+            values[(4 * dimension)] = -2.0;
 
-            LocalHi = new CompressedColumn((2 * spaceDimension) + 1, (2 * spaceDimension) + 1, columnPointers, rowIndices, values);
+            LocalHi = new CompressedColumn((2 * dimension) + 1, (2 * dimension) + 1, columnPointers, rowIndices, values);
 
 
             /******************** Define LocalBi ********************/
@@ -66,7 +64,8 @@ namespace BRIDGES.Solvers.GuidedProjection.QuadraticConstraintTypes
 
 
             /******************** Define Ci ********************/
-            Ci = 0.0;
+
+            Ci = 0d;
         }
 
         #endregion
